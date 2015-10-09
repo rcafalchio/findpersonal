5b5b84705fbc3621fface39ff5c298e3d1ef9d4b
Ricardo <ricardo.cafalchio@gmail.com>
2015-10-09 18:48:53 -0300

    Signed-off-by: Ricardo <ricardo.cafalchio@gmail.com>

--- findpersonalEAR/findpersonalWS/src/main/java/com/findpersonal/findpersonalws/rest/EnvioRest.java
+++ findpersonalEAR/findpersonalWS/src/main/java/com/findpersonal/findpersonalws/rest/EnvioRest.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public abstract class EnvioRest {
 
-	@JsonProperty("apv")
+	@JsonProperty("APV")
 	@NotNull
 	protected double applicationVersion;
 
